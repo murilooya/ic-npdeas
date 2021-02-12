3eb37d46f745e58be2a04b274e89c7bb02f9c528
murilooya <muoyafuso@gmail.com>
2021-02-12 20:06:38 -0300

    hello test serial print, add dht lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin (9800);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello");
+  delay(100);
 }
