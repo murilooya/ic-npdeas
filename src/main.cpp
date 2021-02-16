da519d1963ec004a0b3788a907832611760deb98
murilooya <muoyafuso@gmail.com>
2021-02-16 20:24:22 -0300

    add dht test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
+#include "Adafruit_Sensor.h"
+#include "DHT.h"
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+DHT dht (DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (9800);
+  dht.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello");
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  
   delay(100);
 }
